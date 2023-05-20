dd0bedfb7e66e7530a646aac88e7336af90c9aa5
Benonking <benonkighoma@gmail.com>
2023-05-20 13:44:52 +0300

    delete objt if exixts

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,16 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls:
+            dictionary = dict()
+            for item in FileStorage.__objects.items():
+                if type(item[1]) in [cls]:
+                    dictionary[item[0]] = item[1]
+                else:
+                    continue
+            return dictionary
+        else: 
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
