cad9537622bc981dd7f4a6fd678d7195bc9bb95f
Benonking <benonkighoma@gmail.com>
2023-05-20 12:59:14 +0300

    set up db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,7 @@
 -- script that prepares a mysql server
 -- dbname = hbnb_dev_db host=localhost
 -- passwpord = hbnb_dev_pwd
--- privileges for hbnb_dev = all
--- privileges for hbnb_dev on hbnb_dev_db = select
--- id db already exists script should fail
+
 CREATE SCHEMA IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 CREATE PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
